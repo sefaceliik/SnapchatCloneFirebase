d9a1212ad22e8b80ff9fa345eb9c78476fa1e7a2
sefacelik <sefacelik1800@gmail.com>
2020-05-22 13:17:41 +0300

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Sefa Çelik on 22.05.2020.
+//  Copyright © 2020 Sefa Celik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
